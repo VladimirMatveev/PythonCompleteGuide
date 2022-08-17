1fc26ecd65743e422b5061ec83bc7baf12fa2b2c
mylit <vlmatveev5@gmail.com>
2022-08-17 16:54:28 +0300

    lesson 7

---
+++ lesson 7.py
@@ -0,0 +1,21 @@
+# test = None
+# test = 123
+# print(test)
+#
+# x, y = (1, 5) множественное присв.
+# print(x, y)
+
+#bool
+
+# _true = True
+# _false = False
+#
+# print(type(_true))
+# print(type(_false))
+
+#str(), int(), float(), (bool() # 0, None, '' = False)
+# Функции, приведение переменной к другому типу данных
+x = 5.2
+print(type(x))
+x = int(x)
+print(type(x))
