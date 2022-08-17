ffc0778a0461a68f658bdba0b9b300f84afa1fe7
mylit <vlmatveev5@gmail.com>
2022-08-17 17:29:13 +0300

    lesson 8

---
+++ lesson 8.py
@@ -0,0 +1,37 @@
+# Str
+
+# words = "Hello, 'world'"  # "txt 'txt'"
+#words = "Hello, \"test\" 'world'" # Экранирующий символ
+# words = "Hello, \\test 'world'" # /n - перевод строки /t - табуляция
+# print(words)
+
+
+
+#есть физическая и логическая строка, физическая єто одна строка, логич. это то, что питон воспринимает как одну строчку.
+
+# verse = 'bebebebebe bebebeebbe
+#bebebebe bebebeb ebebbe'
+
+# verse = 'bebebebebe bebebeebbe\
+#bebebebe bebebeb ebebbe'
+
+# verse = 'bebebebebe bebebeebbe\n\
+# bebebebe bebebeb bebbe'
+
+# verse = ('bebebebebe bebebeebbe\n'
+#          'bebebebe bebebeb bebbe')
+#
+# print(verse)
+
+# многостросная строка (3 ковычки ''' ''', """ """)
+verse = '''\
+bebbebebebebe
+beebebebebeb
+beebbeebbebe\
+'''
+print(verse)
+
+
+
+
+
