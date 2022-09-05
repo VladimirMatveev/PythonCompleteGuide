26369e26e46271a25fc39ca8b1de0c5b1bacbe85
mylit <vlmatveev5@gmail.com>
2022-09-05 15:03:43 +0300

    lesson36

---
+++ lesson36.py
@@ -0,0 +1,18 @@
+class A:
+    property1 = 'property 1'
+    property2 = 'property 2'       #свойства класса
+    name = 'guest'
+    def say_hi(self, name=''):             #ключевое слово которое содержит в себе указатель на єкземпляр класса
+        if name:
+            return 'hi, ' + name
+        else:
+            return 'hello, ' + self.name
+
+
+a = A()
+b = A()
+
+# a.property1 = 'property 1'
+# a.property2 = 'property 2'
+print(a.property1)
+print(b.say_hi('John'))
